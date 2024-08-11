3e77952aa23641c36ae924a8d9b9e2e31beca098
VLAD <wint-mil@mail.ru>
2024-08-11 12:22:35 +0300

    AssertJ and Hamcrest.

--- src/main/java/com/gorbunov/junit/service/UserService.java
+++ src/main/java/com/gorbunov/junit/service/UserService.java
@@ -2,9 +2,9 @@ package com.gorbunov.junit.service;
 
 import com.gorbunov.junit.dto.User;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class UserService {
 
@@ -14,7 +14,19 @@ public class UserService {
         return users;
     }
 
-    public boolean add(User user) {
-        return users.add(user);
+    public void add(User... users) {
+        Collections.addAll(this.users, users);
+    }
+
+    public Optional<User> login(String username, String password) {
+        return users.stream()
+                .filter(user -> user.getUsername().equals(username))
+                .filter(user -> user.getPassword().equals(password))
+                .findFirst();
+    }
+
+    public Map<Integer, User> getAllConvertedById() {
+        return users.stream()
+                .collect(Collectors.toMap(User::getId, Function.identity()));
     }
 }
--- src/test/java/com/gorbunov/junit/service/UserServiceTest.java
+++ src/test/java/com/gorbunov/junit/service/UserServiceTest.java
@@ -1,15 +1,24 @@
 package com.gorbunov.junit.service;
 
 import com.gorbunov.junit.dto.User;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.collection.IsEmptyCollection;
+import org.hamcrest.collection.IsMapContaining;
 import org.junit.jupiter.api.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)// PER_METHOD -это стоит по умолчанию. Создается новый обеъект класса UserServiceTest для каждого теста.
 class UserServiceTest {
 
+    private static final User IVAN = User.of(1, "Ivan", "123");
+    private static final User PETR = User.of(2, "Petr", "111");
+    private static final User VLAD = User.of(3, "Vlad", "123");
     private UserService userService;
 
     // должен быть static для TestInstance.Lifecycle.PER_METHOD или менять на PER_CLASS
@@ -20,7 +29,7 @@ class UserServiceTest {
 
     @BeforeAll
     void init() {
-        System.out.println("Before all: "  + this);
+        System.out.println("Before all: " + this);
     }
 
     @BeforeEach
@@ -34,21 +43,69 @@ class UserServiceTest {
     void usersEmptyIfNoUserAdded() {  // В названиях можно использовать snake case
         System.out.println("Test 1: " + this);
         List<User> users = userService.getAll();
+
+        MatcherAssert.assertThat(users, IsEmptyCollection.empty()); // hamcrest
         assertTrue(users.isEmpty(), () -> "User list shouldn't be empty"); // String или Supplier<String> для описания ошибки
     }
 
     @Test
     void usersSizeIfUserAdded() {
         System.out.println("Test 2: " + this);
-        userService.add(new User());
-        userService.add(new User());
+        userService.add(IVAN);
+        userService.add(PETR);
 
         List<User> users = userService.getAll();
-        assertEquals(2, users.size());
+
+        assertThat(users).hasSize(2); // Библиотека AssertJ
+//        assertThat(users).hasSize(2).isEqualTo(userService.getAll()); // Библиотека AssertJ // Можно chaining'ом вызывать несколько проверок
+//        assertEquals(2, users.size());
+    }
+
+    @Test
+    void loginSuccessIfUserExists() {
+        userService.add(IVAN);
+        Optional<User> optionalUser = userService.login(IVAN.getUsername(), IVAN.getPassword());
+
+        assertThat(optionalUser).isPresent(); // Библиотека AssertJ
+        assertThat(optionalUser.get()).isEqualTo(IVAN);
+//        assertTrue(optionalUser.isPresent());
+//        assertEquals(IVAN, optionalUser.get());
+    }
+
+    @Test
+    void userConvertedToMapById() {
+        userService.add(IVAN, PETR);
+
+        Map<Integer, User> users = userService.getAllConvertedById();
+
+        MatcherAssert.assertThat(users, IsMapContaining.hasKey(IVAN.getId())); // Библиотека Hamcrest. Лучше использовать AssertJ.
+        assertAll(
+                () -> assertThat(users).containsKeys(IVAN.getId(), PETR.getId())
+                        .containsKey(IVAN.getId()),
+                () -> assertThat(users).containsKey(IVAN.getId()),
+                () -> assertThat(users).containsValues(IVAN, PETR)
+        );
+
+    }
+
+    @Test
+    void loginFailIfPasswordIsNotCorrect() {
+        userService.add(IVAN);
+        Optional<User> optionalUser = userService.login(IVAN.getUsername(), "incorrect password");
+
+        assertTrue(optionalUser.isEmpty());
+    }
+
+    @Test
+    void loginFailIfPasswordIsDoesNotExists() {
+        userService.add(IVAN);
+        Optional<User> optionalUser = userService.login("Not existed user", "password");
+
+        assertTrue(optionalUser.isEmpty());
     }
 
     @AfterEach
-    void deleteDataFromDB(){
+    void deleteDataFromDB() {
         System.out.println("After each: " + this);
     }
 
